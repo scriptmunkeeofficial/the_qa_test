d1d3c9467e0d3e973863774a7016da1fabd2339b
scriptmunkeeofficial <scriptmunkee@gmail.com>
2020-07-28 21:42:31 -0700

    Removed bookshelf-schema

--- src/models/users-model.js
+++ src/models/users-model.js
@@ -1,36 +1,8 @@
 const db = require('./db-connector');
-const dbSchema = require('bookshelf-schema')
-const Fields = require('bookshelf-schema/lib/fields')
-    , StringField = Fields.StringField
-    , IntField = Fields.IntField
-    , EmailField = Fields.EmailField
-    , BooleanField = Fields.BooleanField
-    , DateTimeField = Fields.DateTimeField
-    , EncryptedStringField = Fields.EncryptedStringField;
 
-const Scope = require('bookshelf-schema/lib/scopes');
-
-db.plugin(dbSchema);
-
-var Users = db.Model.extend({
+var Users = db.model('Users', {
     tableName: 'users',
     hasTimeStamps: true
-},{
-    schema: [
-        // https://bookshelf-schema.readthedocs.io/en/latest/index.html
-        IntField('user_id', { naturalNonZero: true }),
-        StringField('f_name'),
-        StringField('l_name'),
-        StringField('user_name'),
-        EmailField('email'),
-        EncryptedStringField('password', {minLength: 8}),
-        StringField('role'),
-        BooleanField('active'),
-        DateTimeField('created_at'),
-        DateTimeField('updated_at'),
-        //HasMany('Photo'),
-        Scope('isActive', function(){ return this.where({active: true}); })
-    ]
 });
 
 module.exports = Users
